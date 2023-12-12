a3bd89ca5f2d89c48eb39fbd81e1cfb02398a126
seansongss <seansongss@gmail.com>
2023-12-11 21:30:06 -0500

    83 done

--- 83.py
+++ 83.py
@@ -1,8 +1,20 @@
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        curr = head
+        while curr and curr.next:
+            if curr.val == curr.next.val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next
+        
+        return head
         
