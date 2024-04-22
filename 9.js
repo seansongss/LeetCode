3962e43fc8f0500f96dd88430bcb56df6c9763f2
seansongss <seansongss@gmail.com>
2024-04-23 08:40:45 +0900

    Create 9.js

---
+++ 9.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    var reverse = 0;
+    var copy = x;
+
+    while (copy > 0) {
+        const digit = copy % 10;
+        reverse = reverse * 10 + digit;
+        copy = ~~(copy / 10);
+    }
+
+    return reverse == x;
+};
