13959c2339da85e2dc6ed2c2c5f552a65f76e6bf
seansongss <seansongss@github.com>
2024-05-10 15:19:39 +0900

    347_sol wip

---
+++ 347_sol.py
@@ -0,0 +1,9 @@
+from typing import List, Counter
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        return [num for num, _ in counter.most_common(k)]
+    
+a = Solution()
+print(a.topKFrequent([1,1,1,2,2,3], 2))
