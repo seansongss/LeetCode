fec7a3080d9ae3159dc00c61944d24ed54963cd2
seansongss <seansongss@gmail.com>
2024-05-10 16:20:32 +0900

    40 done

--- 49.py
+++ 49.py
@@ -1,34 +1,14 @@
-from typing import List
+from typing import List, DefaultDict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        if len(strs) == 1:
-            return [strs] 
-        result = []
-        count = 0
-        while strs:
-            result.append([])
-            result[count].append(strs[0])
-            str1 = strs[0]
-            strs.remove(str1)
-            to_remove = []
-            for j in strs:
-                if sorted(str1) == sorted(j):
-                    result[count].append(j)
-                    to_remove.append(j)
+        anag_map = DefaultDict(list)
 
-            for j in to_remove:
-                strs.remove(j)
-                
-            count += 1
+        for word in strs:
+            soreted_word = ''.join(sorted(word))
+            anag_map[soreted_word].append(word)
         
-        # if (sorted(strs[0]) == sorted(result[count - 1][0])):
-        #     result[count - 1].append(strs[0])
-        # else:
-        #     result.append([])
-        #     result[count].append(strs[0])
-        
-        return result
+        return list(anag_map.values())
     
 print(Solution().groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
 print(Solution().groupAnagrams(["","", ""]))
