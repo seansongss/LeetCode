a031d344eb248cf9b2eebc8d1213be2192c39086
seansongss <seansongss@gmail.com>
2024-05-10 15:29:47 +0900

    347_sol done

--- 347_sol.py
+++ 347_sol.py
@@ -3,9 +3,8 @@ from typing import List, Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counter = Counter(nums)
+        # .most_common(n) returns a list of n most common elements
+        # ex) Counter([1, 1, 1, 2, 2, 3]).most_common(2) = [(1, 3), (2, 2)]
         return [num for num, _ in counter.most_common(k)]
-    
-a = Solution()
-print(a.topKFrequent([1,1,1,2,2,3], 2))
 
-#sol
+print(Solution().topKFrequent([1, 1, 1, 2, 2, 3], 2))
