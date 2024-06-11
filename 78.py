d1e09768945e04cde324e7c5ef6a8ebcf3b0a0ba
seansongss <seansongss@gmail.com>
2024-06-11 15:41:28 +0900

    78 done backtracking

---
+++ 78.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+
+        def backtrack(start, path):
+            res.append(path)
+            for i in range(start, len(nums)):
+                backtrack(i + 1, path + [nums[i]])
+        
+        backtrack(0, [])
+        
+        return res
