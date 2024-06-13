013a83f632c08f293ffac909b78087ca877b5e08
seansongss <seansongss@gmail.com>
2024-06-13 13:49:41 +0900

    70 done dp

--- 70.py
+++ 70.py
@@ -1,11 +1,26 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        def countStairs(step_left):
-            if step_left == 0:
-                return 1
-            elif step_left < 0:
-                return 0
-            return countStairs(step_left-1) + countStairs(step_left-2)
+        # def countStairs(step_left):
+        #     if step_left == 0:
+        #         return 1
+        #     elif step_left < 0:
+        #         return 0
+        #     return countStairs(step_left-1) + countStairs(step_left-2)
         
-        count = countStairs(n)
-        return count
+        # count = countStairs(n)
+        # return count
+        def solve(n, dp):
+            if n < 0:
+                return 0
+            if n == 0:
+                return 1
+            
+            if dp[n] != -1:
+                return dp[n]
+            
+            dp[n] = solve(n - 1, dp) + solve(n -2, dp)
+
+            return dp[n]
+
+        dp = [-1] * (n+1)
+        return solve(n, dp)
