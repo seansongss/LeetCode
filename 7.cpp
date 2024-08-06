a5931fb2a833822e1e712587245ba2042ea4fd2b
seansongss <seansongss@gmail.com>
2024-08-06 09:16:44 +0900

    faster 7 sol

--- 7.cpp
+++ 7.cpp
@@ -5,12 +5,11 @@ public:
     int reverse(int x) {
         int res = 0;
         while (x != 0) {
-            int digit = x % 10;
             if ((res > INT_MAX / 10) || (res < INT_MIN / 10)) {
                 return 0;
             }
 
-            res = res * 10 + digit;
+            res = res * 10 + x % 10;
             x /= 10;
         }
 
