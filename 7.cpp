a6d1fb480caf88dfbada55261cb64504c0ab5846
seansongss <seansongss@gmail.com>
2024-08-06 09:05:06 +0900

    7 c++

---
+++ 7.cpp
@@ -0,0 +1,19 @@
+#include <string>
+using namespace std;
+class Solution {
+public:
+    int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            if ((res > INT_MAX / 10) || (res < INT_MIN / 10)) {
+                return 0;
+            }
+
+            res = res * 10 + digit;
+            x /= 10;
+        }
+
+        return res;
+    }
+};
