e943cedd69c16556830c7e007fbeb12d182d0d44
seansongss <seansongss@gmail.com>
2024-08-23 10:02:48 +0900

    973 c++ pq

--- 973.cpp
+++ 973.cpp
@@ -2,28 +2,21 @@
 #include <queue>
 #include <cmath>
 using namespace std;
-class Compare {
-public:
-    bool operator() (vector<int> a, vector<int> b) {
-        double dist_a = sqrt(pow(a[0], 2) + pow(a[1], 2));
-        double dist_b = sqrt(pow(b[0], 2) + pow(b[1], 2));
-        
-        return dist_a > dist_b;
-    }
-};
 class Solution {
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<vector<int>, vector<vector<int>>, Compare> heap;
+        priority_queue<pair<int, vector<int>>> pq;
         
         for (vector<int> point : points) {
-            heap.push(point);
+            pq.push({point[0]*point[0] + point[1]*point[1], {point[0], point[1]}});
+            if (pq.size() > k) pq.pop();
         }
 
         vector<vector<int>> res;
         while (k > 0) {
-            res.push_back(heap.top());
-            heap.pop();
+            pair<int, vector<int>> p = pq.top();
+            res.push_back(p.second);
+            pq.pop();
             k--;
         }
         
