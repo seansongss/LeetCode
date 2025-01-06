625e7d010150b10b6028078a4d4b00fc5327bfb0
seansongss <seansongss@gmail.com>
2025-01-06 13:30:51 -0500

    51 wip

---
+++ 51.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        board = []
+        for i in range(n):
+            board.append("." * n)
+        
+        def is_reserved(board, row, col):
+            
+                
+        def plot_queen(board, row, col, plotted_queens):
+            
+            
+        
