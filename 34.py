e8121c24d9a2cd789e19c285d587d249fa562fa2
seansongss <seansongss@gmail.com>
2025-01-07 15:48:28 -0500

    34 wip

---
+++ 34.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        def bs(start, end, nums, target):
+            
+            
+
+        
+        
