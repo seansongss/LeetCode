5ffa1bc98c7eb49447375a1654c580094d27a250
Sean Song <seansongss@gmail.com>
2025-01-08 05:40:29 -0500

    34 binary search

--- 34.py
+++ 34.py
@@ -5,9 +5,26 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        def bs(start, end, nums, target):
-            
-            
+        def bs(nums, target, findStart):
+            l, r = 0, len(nums) - 1
+            pos = -1
 
+            while l <= r:
+                mid = (l + r) // 2
+                if nums[mid] == target:
+                    pos = mid
+                    if findStart:
+                        r = mid - 1
+                    else:
+                        l = mid + 1
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+                
+            return pos
         
-        
+        start_pos = bs(nums, target, True)
+        end_pos = bs(nums, target, False)
+
+        return [start_pos, end_pos]
