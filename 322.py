8db2160004dfc55358cb981a224804779b6d693a
seansongss <seansongss@gmail.com>
2025-12-15 16:00:48 +0900

    322: DP med

---
+++ 322.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        memo = {}
+
+        def dfs(amount):
+            if amount == 0:
+                return 0
+            if amount in memo:
+                return memo[amount]
+            
+            res = 1e9
+            for coin in coins:
+                if amount - coin >= 0:
+                    res = min(res, 1+dfs(amount - coin))
+            
+            memo[amount] = res
+            return res
+
+        res = dfs(amount)
+        return -1 if res >= 1e9 else res
